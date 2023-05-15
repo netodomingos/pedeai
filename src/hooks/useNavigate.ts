5bf330f03ac796d0c38c880d92479bcf10fb09ae
geraldo_domingos <geraldoneto@locus.software>
2023-05-14 21:43:32 -0300

    ✨feature: navigation with bottom tabs

--- src/hooks/useNavigate.ts
+++ src/hooks/useNavigate.ts
@@ -1,6 +1,7 @@
 
 import { ParamListBase, useNavigation, CommonActions} from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack'
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
 
 function useNavigate() {
     const { navigate } = useNavigation<StackNavigationProp<ParamListBase>>();
@@ -14,4 +15,10 @@ function useDispatch(){
     return (route: string, params?: { }) => dispatch(CommonActions.reset({ index: 0, routes: [{ name: route, params: params }] }))
 }
 
-export { useNavigate, useDispatch }
+function useNavigateBottomTabs() {
+    const { jumpTo } = useNavigation<BottomTabNavigationProp<ParamListBase>>();
+    
+    return (route: string, params?: { }) => jumpTo(route, params)
+};
+
+export { useNavigate, useDispatch, useNavigateBottomTabs}
