fd00d25b0921662ee62c52be86ebb2c57a98986b
geraldo_domingos <geraldoneto@locus.software>
2023-05-16 18:31:19 -0300

    ✨feature: create a header component

---
+++ src/components/header.tsx
@@ -0,0 +1,29 @@
+import { Platform, StatusBar, StyleSheet, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import { Feather } from '@expo/vector-icons';
+import { Colors } from '../constants/colors';
+import { IButton } from '../interfaces/IButton';
+
+export default function Header({onClick}: IButton) {
+  return (
+    <View style={styles.container}>
+        <TouchableOpacity style={styles.buttonBack} onPress={onClick}>
+            <Feather name="arrow-left" size={24} color={Colors.primary} />    
+        </TouchableOpacity>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+        borderBottomWidth: 1.5,
+        borderBottomColor: Colors.disabled,
+        paddingHorizontal: 5,
+    },
+    buttonBack: {
+        width: 40,
+        paddingVertical: 3
+    }
+  });
