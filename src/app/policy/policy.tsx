c2233c2435c0d36656e489e3167a458f6d33f1ed
geraldo_domingos <geraldoneto@locus.software>
2023-05-16 18:32:20 -0300

    ✨feature: create terms and policy screens

--- src/app/policy/policy.tsx
+++ src/app/policy/policy.tsx
@@ -1,10 +1,50 @@
-import { View, Text } from 'react-native'
+import { Text, SafeAreaView, ScrollView, StyleSheet } from 'react-native'
 import React from 'react'
+import Header from '../../components/header'
+
+import { policy } from '../../constants/terms_and_policys'
+import { Colors } from '../../constants/colors'
+import { useNavigateBottomTabs } from '../../hooks/useNavigate'
 
 export default function Policy() {
+  const navigate = useNavigateBottomTabs()
+  
   return (
-    <View>
-      <Text>policy</Text>
-    </View>
+    <>
+      <Header onClick={() => navigate('Perfil')} />
+      <SafeAreaView style={styles.container}>
+        <ScrollView>
+          <Text style={styles.title}>{policy.title}</Text>
+          <Text style={styles.date}>Data da última atualização: {policy.last_update}</Text>
+          <Text style={styles.content}>{policy.content}</Text>
+        </ScrollView>
+      </SafeAreaView>
+    </>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 8,
+  },
+  date: {
+    marginTop: 20,
+    textAlign: 'left',
+    fontSize: 14,
+    fontFamily: 'Regular',
+    color: Colors.font
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontFamily: 'Regular',
+    color: Colors.font
+  },
+  content: {
+    textAlign: 'left',
+    fontSize: 14,
+    fontFamily: 'Regular',
+    color: Colors.font
+  }
+})
--- src/app/terms/terms.tsx
+++ src/app/terms/terms.tsx
@@ -1,10 +1,50 @@
-import { View, Text } from 'react-native'
+import { Text, SafeAreaView, ScrollView, StyleSheet } from 'react-native'
 import React from 'react'
+import Header from '../../components/header'
+
+import { terms } from '../../constants/terms_and_policys'
+import { Colors } from '../../constants/colors'
+import { useNavigateBottomTabs } from '../../hooks/useNavigate'
 
 export default function Terms() {
+ const navigate = useNavigateBottomTabs()
+
   return (
-    <View>
-      <Text>terms</Text>
-    </View>
+    <>
+      <Header onClick={() => navigate('Perfil')} />
+      <SafeAreaView style={styles.container}>
+        <ScrollView>
+          <Text style={styles.title}>{terms.title}</Text>
+          <Text style={styles.subTitle}>{terms.subTitle}</Text>
+          <Text style={styles.content}>{terms.content}</Text>
+        </ScrollView>
+      </SafeAreaView>
+    </>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 8,
+  },
+  subTitle: {
+    marginTop: 20,
+    textAlign: 'left',
+    fontSize: 14,
+    fontFamily: 'SemiBold',
+    color: Colors.font
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontFamily: 'Regular',
+    color: Colors.font
+  },
+  content: {
+    textAlign: 'left',
+    fontSize: 14,
+    fontFamily: 'Regular',
+    color: Colors.font
+  }
+})
