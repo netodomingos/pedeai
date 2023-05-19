1ba9d87004c5e7297e6a672a40cb07fb22ac039b
geraldo_domingos <geraldoneto@locus.software>
2023-05-19 01:53:14 -0300

    ✨feature: app configure

--- App.tsx
+++ App.tsx
@@ -3,24 +3,31 @@ import { useFonts } from 'expo-font'
 
 import Toast from 'react-native-toast-message';
 
-import { AuthProvider } from './src/context/tokenContext';
+import AuthContext, { AuthProvider } from './src/context/tokenContext';
 
-import Index from './index';
 import { Colors } from './src/constants/styles';
+import { useContext } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AuthRoutes from './src/routes/Auth.routes';
+import Routes from './src/routes/Routes';
 
 export default function App() {
+  const { loading, isLogged } = useContext(AuthContext) 
   const [fontsLoaded] = useFonts({
     'Regular': require('./assets/fonts/Nunito-Regular.ttf'),
     'SemiBold': require('./assets/fonts/Nunito-SemiBold.ttf'),
   });
 
-  if (!fontsLoaded) {
+  if (!fontsLoaded || loading === true) {
     return null;
   } else {
+    
     return (
       <AuthProvider>
         <StatusBar style="auto" backgroundColor={Colors.primary}  />
-        <Index />
+          <NavigationContainer>
+            {isLogged === true ? <AuthRoutes /> : <Routes />}
+          </NavigationContainer>
         <Toast />
       </AuthProvider>
     );
